d3d43f4d8e98c06fe516b8de5b69e55328a59302
emirdmrgzr <emir.demirgezer@hotmail.com>
2023-03-10 09:16:54 +0300

    Car value part became visible

--- src/components/CarValue.js
+++ src/components/CarValue.js
@@ -1,5 +1,20 @@
+import { useSelector } from "react-redux";
+
 export default function CarValue(){
+   const totalCost = useSelector((state) =>{
+      const filteredCars = state.cars.data.filter((car)=>
+      car.name.toLowerCase().includes(state.cars.searchTerm.toLowerCase())
+      );
+   let cost = 0;
+   for(let car of filteredCars){
+      cost += car.cost;
+      }
+   return cost
+   });
+
    return (
-   <div>Car Value</div>
+   <div className="car-value">
+      Total Cost: ${totalCost}
+   </div>
    );
 }
